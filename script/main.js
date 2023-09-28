cfcb5be2d9eeb45211344f3f86a9299a9e4bd674
AyaExpTech <ayagawap@gmail.com>
2023-09-28 13:45:03 +0900

    ？？

--- script/main.js
+++ script/main.js
@@ -18,3 +18,17 @@ const $$ = selector => document.querySelectorAll(selector);
 window.addEventListener("load", () => {
     if (window.location.hash) window.location.replace(window.location.hash);
 });
+
+/**
+ * 各種、変数になりそうなやつ
+ */
+const Options = {
+    "laneWidth": 50, // レーン幅(px)
+};
+
+/**
+ * 画面リサイズ時、canvasの縦幅を自動的に設定
+ */
+window.addEventListener("resize", event => {
+    $("#view").setAttribute("height", $("#view").clientHeight - 1);
+})
